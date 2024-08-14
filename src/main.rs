d69fb4b7f25d76547502d95226029007f5267d30
June Park <junepark202012@gmail.com>
2024-08-14 17:35:55 +0900

    feat: Set up config

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use zero2prod::configuration::get_configuration;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:0")?;
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = std::net::TcpListener::bind(address)?;
     run(listener)?.await
 }
