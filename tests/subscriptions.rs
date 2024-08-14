837969f92082f597bc76f298fa777caffcdf6032
June Park <junepark202012@gmail.com>
2024-08-14 17:47:56 +0900

    feat: connect to db

--- tests/subscriptions.rs
+++ tests/subscriptions.rs
@@ -1,10 +1,20 @@
+use sqlx::{Connection, PgConnection};
+
+use zero2prod::configuration::get_configuration;
+
 mod utils;
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = utils::spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
+    // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
         .post(&format!("{}/subscriptions", &app_address))
@@ -14,7 +24,16 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .await
         .expect("Failed to execute request.");
 
+    // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
