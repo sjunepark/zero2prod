08b828259104d9a8f732299cd5c8fed50aae277d
June Park <junepark202012@gmail.com>
2024-08-15 15:39:20 +0900

    feat: use std instead of once_cell

--- tests/utils.rs
+++ tests/utils.rs
@@ -1,13 +1,13 @@
 use std::net::TcpListener;
+use std::sync::LazyLock;
 
-use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
-static TRACING: Lazy<()> = Lazy::new(|| {
+static TRACING: LazyLock<()> = LazyLock::new(|| {
     let default_filter_level = "info".to_string();
     let subscriber_name = "test".to_string();
     if std::env::var("TEST_LOG").is_ok() {
@@ -22,7 +22,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 /// Spin up an instance of our application
 /// and return its address (i.e. http://localhost:XXXX)
 pub async fn spawn_app() -> TestApp {
-    Lazy::force(&TRACING);
+    *TRACING;
 
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
     let port = listener.local_addr().unwrap().port();
